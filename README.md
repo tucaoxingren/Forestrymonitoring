a76be53c83aeba7c445ce6f93643155601d4eb68
吐槽星人 <tucaoxingren@gmail.com>
2017-09-22 13:28:58 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Forestrymonitoring
 使用树莓派等智能硬件监测森林，然后将监测结果展示到手机APP
+初始界面截图
+![v1.0](Screenshot\device-2017-09-22-125911.png"v1.0")
--- app/src/main/java/com/example/forestrymonitoring/MainActivity.java
+++ app/src/main/java/com/example/forestrymonitoring/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.forestrymonitoring;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+    }/*
+    class MybuttonLinsentent implements OnClickListener{
+
+        void onClick(DialogInterface var1, int var2){
+
+        }
+    }*/
 }
